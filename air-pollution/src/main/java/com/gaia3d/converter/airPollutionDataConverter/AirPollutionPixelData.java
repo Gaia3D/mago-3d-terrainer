ca9ecac32379c5534751dd15067c53d66eb01e0c
znkim <znkim@gaia3d.com>
2025-09-18 16:51:49 +0900

    refactor: improve whole structure - improve build script and namming - remove hardcoding

--- air-pollution/src/main/java/com/gaia3d/converter/airPollutionDataConverter/AirPollutionPixelData.java
+++
@@ -1,8 +0,0 @@
-package com.gaia3d.converter.airPollutionDataConverter;
-
-public class AirPollutionPixelData {
-    public double X;
-    public double Y;
-    public double Z;
-    public double averageConcentration;
-}
